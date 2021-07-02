100526159e7a082d143762829e9abd1a4f1a2f72
Stephanie Sanchez <stephanie.sanchez@theksquaregroup.com>
2021-07-01 21:02:18 -0500

    Without encryption

--- jpa_jwt/src/main/java/com/security/steff/jpa_jwt/security/SecurityConfiguration.java
+++ jpa_jwt/src/main/java/com/security/steff/jpa_jwt/security/SecurityConfiguration.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -50,7 +52,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return NoOpPasswordEncoder.getInstance(); //no encoding
+        //return new BCryptPasswordEncoder();
     }
 
     @Bean
